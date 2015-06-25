9acf14d95d19527322ad1c345141700d8a2c4764
Kris Selden <kris.selden@gmail.com>
2015-06-25 16:37:09 -0700

    quick dirty reuse of cells, much refactoring to do

--- addon/components/ember-list.js
+++ addon/components/ember-list.js
@@ -20,6 +20,10 @@ class Cell {
   }
 }
 
+function formatStyle(pos, width, height) {
+  return 'position: absolute; top: 0; left: 0; -webkit-transform: translate('+pos.x+'px, '+pos.y+'px); width: '+width+'px; height: '+height+'px;';
+}
+
 export default Ember.Component.extend({
   layout: layout,
   init() {
@@ -33,6 +37,7 @@ export default Ember.Component.extend({
     this.height = 0;
     this.contentElement = undefined;
     this.cells = [];
+    this.cellMap = Object.create(null);
   },
   didInitAttrs() {
     this.offsetX = this.attrs['offset-x'] | 0;
@@ -62,21 +67,71 @@ export default Ember.Component.extend({
   },
   willRender() {
     this.cellLayout.length = this.getAttr('items').length;
+
+    var priorMap = this.cellMap;
+    var cellMap = Object.create(null);
+
     var index = this.cellLayout.indexAt(this.offsetX, this.offsetY, this.width, this.height);
     var count = this.cellLayout.count(this.offsetX, this.offsetY, this.width, this.height);
     var items = this.getAttr('items');
-    this.set('cells', []);
-    for (var i=0; i<count; i++) {
-      var pos = this.cellLayout.positionAt(index+i, this.width, this.height);
-      var width = this.cellLayout.widthAt(index+i, this.width, this.height);
-      var height = this.cellLayout.heightAt(index+i, this.width, this.height);
-      this.cells.push(new Cell(i,items[index + i], index+i,
-        'position: absolute; top: 0; left: 0; transform: translate('+pos.x+'px, '+pos.y+'px); width: '+width+'px; height: '+height+'px;'));
+    var i, pos, width, height, style, itemIndex, cell;
+
+    var newItems = [];
+
+    for (i=0; i<count; i++) {
+      itemIndex = index+i;
+
+      cell = priorMap[itemIndex];
+      if (priorMap[itemIndex]) {
+        // TODO don't assume item index is a stable key
+        // this is just quick and dirty code at the moment to see dom reuse
+        pos = this.cellLayout.positionAt(itemIndex, this.width, this.height);
+        width = this.cellLayout.widthAt(itemIndex, this.width, this.height);
+        height = this.cellLayout.heightAt(itemIndex, this.width, this.height);
+        style = formatStyle(pos, width, height);
+        Ember.set(cell, 'style', style);
+        cellMap[itemIndex] = cell;
+      } else {
+        newItems.push(itemIndex);
+      }
     }
+
+    for (i=0; i<this.cells.length; i++) {
+      cell = this.cells[i];
+      if (!cellMap[cell.index]) {
+        if (newItems.length) {
+          itemIndex = newItems.pop();
+          pos = this.cellLayout.positionAt(itemIndex, this.width, this.height);
+          width = this.cellLayout.widthAt(itemIndex, this.width, this.height);
+          height = this.cellLayout.heightAt(itemIndex, this.width, this.height);
+          style = formatStyle(pos, width, height);
+          Ember.set(cell, 'style', style);
+          Ember.set(cell, 'index', itemIndex);
+          Ember.set(cell, 'item', items[itemIndex]);
+          cellMap[itemIndex] = cell;
+        } else {
+          Ember.set(cell, 'style', 'height: 0; display: none;');
+        }
+      }
+    }
+
+    for (i=0; i<newItems.length; i++) {
+      itemIndex = newItems[i];
+      pos = this.cellLayout.positionAt(itemIndex, this.width, this.height);
+      width = this.cellLayout.widthAt(itemIndex, this.width, this.height);
+      height = this.cellLayout.heightAt(itemIndex, this.width, this.height);
+      style = formatStyle(pos, width, height);
+      cell = new Cell(this.cells.length, items[itemIndex], itemIndex, style);
+      cellMap[itemIndex] = cell;
+      this.cells.push(cell);
+    }
+
+    this.cellMap = cellMap;
   },
   initBounds() {
     // TODO measure clientWidth and clientHeight vs offsetWidth and offsetHeight
-    this.element.style.overflow = 'auto';
+    this.element.style.overflow = 'scroll';
+    this.element.style.webkitOverflowScrolling = 'touch';
     this.element.style.position = 'relative';
     this.element.style.boxSizing = 'border-box';
     if (this.width > 0) {
