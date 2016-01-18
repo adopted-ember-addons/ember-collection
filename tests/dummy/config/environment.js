4d24bde207d7a4e6c25e94396f4a4b18b572eef9
Edward Faulkner <ef@alum.mit.edu>
2016-01-17 18:06:47 -0600

    automated deploys to github pages
    This lets us deploy the demo to GitHub Pages with a single command.
    
    Required first-time setup: create a `gh-pages` branch with
    
        git branch --orphan gh-pages
        git commit --allow-empty
        git push -u origin gh-pages
    
    After that, anyone with commit privleges to this repo can deploy on demand with `ember deploy production` and view the app at http://emberjs.github.io/ember-collection/

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -44,7 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.baseURL = '/ember-collection';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
