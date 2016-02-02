b97ecd3c5eb2c9aa6b10997f086f98b8e192db37
Robert Jackson <robert.w.jackson@me.com>
2016-02-01 21:05:01 -0500

    Remove template compiler from built assets.
    We do not currently need this, and there are better ways of doing this
    that do not involve compiling templates in the browser (i.e.
    `htmlbars-inline-precompiler`).

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,13 +6,5 @@ module.exports = function(defaults) {
     // Add options here
   });
 
-  /*
-    This build file specifes the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
-  app.import('bower_components/ember/ember-template-compiler.js');
-
   return app.toTree();
 };
