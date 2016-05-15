93a3734c706b2d04a6bb20129d2c1a44d9281821
Alex Ford <alexford@frontside.io>
2016-05-15 00:06:04 -0500

    Added ember-cli-github-pages addon

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -41,6 +41,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.baseURL = '/ember-collection/';
     ENV.baseURL = '/ember-collection';
     ENV.locationType = 'hash';
   }
