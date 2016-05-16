5f7a0a9459bde56759d56df29c5c77eddb4ff86a
Alex Ford <alexford@frontside.io>
2016-05-16 16:39:56 -0500

    Add Syntax Highlighting

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,6 +13,7 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
+  app.import(app.bowerDirectory + '/highlightjs/styles/tomorrow.css');
 
   return app.toTree();
 };
