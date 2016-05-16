986be581b788475625d50e7929019badedffe485
Alex Ford <alexford@frontside.io>
2016-05-16 10:44:09 -0500

    Remove mixin and add grid interface

--- tests/dummy/app/controllers/scroll-position.js
+++ tests/dummy/app/controllers/scroll-position.js
@@ -1,44 +1,5 @@
 import Ember from 'ember';
-import Grid from 'ember-collection/layouts/grid';
-
-class GridInterface extends Grid {
-  get length() {
-    return this.size;
-  }
-  set length (num) {
-    Ember.set(this, 'size', num);
-  }
-
-  contentSize(w, h) {
-    let result = Grid.prototype.contentSize.call(this, w, h);
-    let args = Array.prototype.slice.call(arguments);
-    Ember.set(this, '_contentSize', {args: args, result: result});
-    console.log(`contentSize(${w}, ${h}) => `, result);
-    return result;
-  }
-
-  indexAt(x, y, w, h) {
-    let result = Grid.prototype.indexAt.call(this, x, y, w, h);
-    let args = Array.prototype.slice.call(arguments);
-    Ember.set(this, '_indexAt', {args: args, result: result});
-    console.log(`indexAt(${x}, ${y}, ${w}, ${h}) => `, result);
-    return result;
-  }
-
-  count(x, y, w, h) {
-    let result = Grid.prototype.count.call(this, x, y, w, h);
-    let args = Array.prototype.slice.call(arguments);
-    Ember.set(this, '_count', {args: args, result: result});
-    console.log(`count(${x}, ${y}, ${w}, ${h}) => `, result);
-    return result;
-  }
-
-  formatItemStyle(itemIndex, clientWidth, clientHeight) {
-    let formatItemStyle = Grid.prototype.formatItemStyle.call(this, itemIndex, clientWidth, clientHeight);
-    console.log(`formatItemStyle(${itemIndex}, ${clientWidth}, ${clientHeight}) => `, formatItemStyle);
-    return formatItemStyle;
-  }
-}
+import { FixedGridInterface } from '../interface/grid-interface';
 
 export default Ember.Controller.extend({
   itemWidth: 100,
@@ -93,66 +54,14 @@ export default Ember.Controller.extend({
     }
   },
 
-  grid: Ember.computed('itemWidth', 'itemHeight', function() {
-    return new GridInterface(this.get('itemWidth'), this.get('itemHeight'));
+  fixedGrid: Ember.computed('itemWidth', 'itemHeight', function() {
+    return new FixedGridInterface(this.get('itemWidth'), this.get('itemHeight'));
   }),
-
-  markdown: Ember.computed('grid.size', 'grid._contentSize', 'grid._indexAt', 'grid._count', function() {
-    if(this.get('grid.size') < 1){return '';}
-    return this.getGridContentSize() + '\n' +
-      this.getGridIndexAt() + '\n' +
-      this.getGridCount();
-  }),
-
-  /*
-   Reactive Javascript Markdown Strings
-   TODO: Style this better or move to a mixin?
-  */
-
-  getGridContentSize() {
-    let result = Grid.prototype.contentSize.apply(
-      this.get('grid'), this.get('grid._contentSize.args')
-    );
-    return `\n\
-/**
- * Size of the content area
- *
- * contentSize(containerWidth, containerHeight)
- */
- contentSize: function(${this.get('grid._contentSize.args').join(', ')}) {
-   return ${this.toString(result)};
- }`;
-  },
-
-  getGridIndexAt() {
-    let result = Grid.prototype.indexAt.apply(
-      this.get('grid'), this.get('grid._indexAt.args')
-    );
-    return `/**
- * Index of the first visible item
- *
- * indexAt(offsetX, offsetY, containerWidth, containerHeight)
- */
- indexAt: function(${this.get('grid._indexAt.args').join(', ')}) {
-   return ${this.toString(result)};
- }`;
-  },
-
-  getGridCount() {
-    let result = Grid.prototype.count.apply(
-      this.get('grid'), this.get('grid._count.args')
-    );
-    return `/**
- * Return the number of items to display
- *
- * count(offsetX, offsetY, containerWidth, containerHeight)
- */
- count: function(${this.get('grid._count.args').join(', ')}) {
-   return ${this.toString(result)};
- }`;
-  },
-
-  toString(result) {
-    return JSON.stringify(result).replace(/:/g, ': ').replace(/"/g, '');
-  }
+  markdown: Ember.computed('fixedGrid._size', 'fixedGrid._contentSize', 'fixedGrid._indexAt', 'fixedGrid._count', function() {
+    if(!(this.get('fixedGrid._size') || 0)){ return ''; }
+    return '\n' +
+      this.get('fixedGrid.markdownContentSize') + '\n' +
+      this.get('fixedGrid.markdownIndexAt') + '\n' +
+      this.get('fixedGrid.markdownCount');
+  })
 });
