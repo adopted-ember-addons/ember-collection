6b5e44d0f0b4da191d178e8a810acdb18750aadc
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-10-23 17:43:38 +0200

    Remove CircleCI config

--- testem.js
+++ testem.js
@@ -1,18 +1,9 @@
 /* jshint node:true */
 
-var fs = require('fs');
-var path = require('path');
-var existsSync = require('exists-sync');
-var mkdirp = require('mkdirp');
-
-var isCI = !!process.env['CI'];
-
 var options = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
-  "xunit_intermediate_output": true,
-  "report_file": "test-results.xml",
   "launch_in_ci": [
     "Chrome",
     "Firefox",
@@ -41,16 +32,5 @@ var options = {
     },
   }
 };
-
-if (isCI && !process.env['TRAVIS']) {
-  var testReportsPath = path.join(process.env['CIRCLE_TEST_REPORTS'], process.env.SCENARIO_GROUP);
-
-  if (!existsSync(testReportsPath)) {
-    mkdirp.sync(testReportsPath);
-  }
-
-  options['reporter'] = 'xunit';
-  options['report_file'] = path.join(testReportsPath, process.env.EMBER_TRY_CURRENT_SCENARIO + '.xml');
-}
 
 module.exports = options;
