b9c8813cc7228b8f3a97723a10888bdff4aa97f3
Robert Wagner <rwwagner90@gmail.com>
2020-04-06 13:25:56 -0400

    Fix template lint

---
+++ .template-lintrc.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  extends: 'recommended',
+  rules: {
+    'no-inline-styles': false,
+    'no-quoteless-attributes': false,
+    'no-triple-curlies': false,
+    'require-button-type': false,
+    'style-concatenation': false
+  }
+};
