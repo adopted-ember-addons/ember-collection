9e14231d6f49fa7d57c68eec8fef22a857683292
Gaurav Munjal <Gaurav0@aol.com>
2020-05-11 22:42:54 -0400

    fix total-height test

--- tests/unit/total-height-test.js
+++ tests/unit/total-height-test.js
@@ -1,4 +1,3 @@
-import { run } from '@ember/runloop';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
@@ -8,39 +7,33 @@ import template from '../templates/fixed-grid';
 module('totalHeight', function(hooks) {
   setupRenderingTest(hooks);
 
-  test("single column", function(assert) {
+  test("single column", async function(assert) {
     var width = 50, height = 500, itemHeight = 50, itemWidth = 50;
     var content = generateContent(20);
 
-    run(async () => {
-      this.setProperties({ width, height, itemWidth, itemHeight, content });
-      await render(template);
-    });
+    this.setProperties({ width, height, itemWidth, itemHeight, content });
+    await render(template);
 
-    assert.equal(findContainer(this).height(), 1000);
+    assert.equal(findContainer(this.element).getBoundingClientRect().height, 1000);
   });
 
-  test("even", function(assert) {
-    var width = 100, height = 500, itemHeight = 50, itemWidth = 50;
+  test("even", async function(assert) {
+    var width = 120, height = 500, itemHeight = 50, itemWidth = 50;
     var content = generateContent(20);
 
-    run(async () => {
-      this.setProperties({ width, height, itemWidth, itemHeight, content });
-      await render(template);
-    });
+    this.setProperties({ width, height, itemWidth, itemHeight, content });
+    await render(template);
 
-    assert.equal(findContainer(this).height(), 500);
+    assert.equal(findContainer(this.element).getBoundingClientRect().height, 500);
   });
 
-  test("odd", function(assert) {
-    var width = 100, height = 500, itemHeight = 50, itemWidth = 50;
+  test("odd", async function(assert) {
+    var width = 120, height = 500, itemHeight = 50, itemWidth = 50;
     var content = generateContent(21);
 
-    run(async () => {
-      this.setProperties({ width, height, itemWidth, itemHeight, content });
-      await render(template);
-    });
+    this.setProperties({ width, height, itemWidth, itemHeight, content });
+    await render(template);
 
-    assert.equal(findContainer(this).height(), 550);
+    assert.equal(findContainer(this.element).getBoundingClientRect().height, 550);
   });
 });
